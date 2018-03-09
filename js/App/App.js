b51dd714513e131dac9d23e4b16a3b1405d67489
frozen4code <bhpndrsingh306@gmail.com>
2018-03-09 22:45:55 +0530

    Added React Router 4 and Routes

--- js/App/App.js
+++ js/App/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
-const App = () => {
-    return(
-        <div>
-            <h1>Image Gallery App</h1>
-        </div>
-    );
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {Home, About, Contact, Help} from './components';
+class App extends React.Component{
+    render(){
+        return(
+            
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/home' component={Home} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/contact' component={Contact} />
+                    <Route exact path='/help' component={Help} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
+
 export default App;
